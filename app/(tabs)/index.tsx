2780d4f35cfb643da55b8e72e8e1983d7df379b2
Max <maxx68329@gmail.com>
2025-09-06 11:00:27 -0700

    create turns

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,25 +1,28 @@
 import { Image } from "expo-image";
-import { Button, Platform, StyleSheet } from "react-native";
+import { Button, StyleSheet } from "react-native";
 
-import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { useState } from "react";
 
 export default function HomeScreen() {
   const [board, setBoard] = useState(
-    [...Array(3)].map(() => [...Array(3)].map(() => 0))
+    [...Array(3)].map(() => [...Array(3)].map(() => " "))
   );
-
+  const [turn, setTurn] = useState(
+    "X"
+  )
   const handleClick = (i: number, j: number) => {
     setBoard((prev) => {
-      prev = [...prev];
-      prev[i][j]++;
+      prev = prev.map((row) => [...row]);
+      
+      prev[i][j] = turn;
       return prev;
     });
+    setTurn((prev) => {
+      return prev==="X"? "O":"X"
+    })
   };
-  console.log(board);
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
@@ -30,60 +33,19 @@ export default function HomeScreen() {
         />
       }
     >
-    {board.map((row, i) =>
-      <ThemedView style={styles.titleContainer}>
-        {row.map((cell, j) => (
-          <Button
-            title={"Click me " + cell}
-            key={i}
-            onPress={() => {
-              handleClick(i, j);
-            }}
-          />
-        )) }
-      </ThemedView>
-    )}
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-        <HelloWave />
-        {[...Array(10)].map((_, index) => (
-          <HelloWave key={index} />
-        ))}
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          to see changes. Press{" "}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: "cmd + d",
-              android: "cmd + m",
-              web: "F12",
-            })}
-          </ThemedText>{" "}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">
-            npm run reset-project
-          </ThemedText>{" "}
-          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
-          directory. This will move the current{" "}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+      {board.map((row, i) => (
+        <ThemedView style={styles.titleContainer}>
+          {row.map((cell, j) => (
+            <Button
+              title={cell}
+              key={i}
+              onPress={() => {
+                handleClick(i, j);
+              }}
+            />
+          ))}
+        </ThemedView>
+      ))}
     </ParallaxScrollView>
   );
 }
