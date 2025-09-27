a010f17436ae87e5c413da4bc0d24892f3c1ae67
Max <maxx68329@gmail.com>
2025-09-27 10:47:53 -0700

    finish game

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,6 +4,7 @@ import { Button, StyleSheet } from "react-native";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedView } from "@/components/ThemedView";
 import { useState } from "react";
+import { ThemedText } from "@/components/ThemedText";
 
 export default function HomeScreen() {
   const [board, setBoard] = useState(
@@ -13,16 +14,65 @@ export default function HomeScreen() {
     "X"
   )
   const handleClick = (i: number, j: number) => {
-    setBoard((prev) => {
-      prev = prev.map((row) => [...row]);
-      
-      prev[i][j] = turn;
-      return prev;
-    });
+    const boardCopy = board.map((row) => [...row]);
+    boardCopy[i][j] = turn;
+    setBoard(boardCopy)
     setTurn((prev) => {
       return prev==="X"? "O":"X"
     })
+    console.log("isDraw: ", isDraw(boardCopy))
+    console.log("isWin: ", isWin(boardCopy))
+
   };
+
+  const isWin = (board: string[][]) => {
+    // checkrows
+    for (const row of board) {
+      const allSame = row.every(e => e !== " " && e === row[0])
+      if (allSame) {
+        return true
+      }
+    }
+
+    // checkcolumns
+    for (let i=0; i < board.length; i++) {
+      let sameChars = true
+      for (let j=0; j < board[i].length - 1; j++) {
+        if ((board[j][i] !== board[j+1][i] || board[j][i] === " ")) {
+          sameChars = false
+        }
+      }
+      if (sameChars === true) {
+        return true
+      }
+    }
+
+    // check diagonal1
+    if (board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[0][0] !== " ") {
+      return true
+    }
+
+    //check diagonal2
+    if (board[0][2] === board[1][1] && board[1][1] === board[2][0] && board[1][1] !== " ") {
+      return true
+    }
+
+    return false
+  }
+
+  const isFull = (board: string[][]) => {
+    for (const row of board) {
+      const allFull = row.every(e => e !== " ")
+      if (!allFull) {
+        return false
+      }
+    }
+    return true
+  }
+
+  const isDraw = (board: string[][]) => !isWin(board) && isFull(board)
+
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
@@ -34,18 +84,33 @@ export default function HomeScreen() {
       }
     >
       {board.map((row, i) => (
-        <ThemedView style={styles.titleContainer}>
+        <ThemedView style={styles.titleContainer} key={i}>
           {row.map((cell, j) => (
-            <Button
-              title={cell}
-              key={i}
+            <ThemedText style = {styles.cell}><Button
+              disabled={cell==="X" || cell==="O" || isWin(board)}
+              
+              title={cell === " "?"_":cell}
+              
               onPress={() => {
                 handleClick(i, j);
               }}
-            />
+            /></ThemedText>
+
           ))}
+          
         </ThemedView>
       ))}
+      <ThemedView>
+        {(isWin(board)) ? "game won" : isDraw(board) ? "draw" : ""}
+      </ThemedView>
+      <Button 
+      title={"play again"} 
+      onPress={() => {
+        setBoard([...Array(3)].map(() => [...Array(3)].map(() => " ")))
+        setTurn("X")
+      }}
+      />
+
     </ParallaxScrollView>
   );
 }
@@ -67,4 +132,9 @@ const styles = StyleSheet.create({
     left: 0,
     position: "absolute",
   },
+  cell: {
+    height: 30,
+    width: 30,
+    fontFamily: "monospace"
+  }
 });
